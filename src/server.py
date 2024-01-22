b198d8c1e159637b305747e4ed4dca146729f477
MikkelMohrHansen <147592404+MikkelMohrHansen@users.noreply.github.com>
2024-01-22 14:15:39 +0100

    added alarm kald

--- src/server.py
+++ src/server.py
@@ -90,7 +90,7 @@ class DataHandler():
         try:
             self.mycursor.execute('INSERT INTO tempreadings (device_id, temp_pipe, temp_room) VALUES (%s,%s,%s)', (device_id, temp_pipe, temp_room))
             self.db.commit()
-
+            self.alarm(device_id)
             print('[+] Successfully added Temperature entry for %s \n' % device_id)
         except Exception as e:
             print('[!] Encountered exception error while adding temperature entry: %s' % e)
